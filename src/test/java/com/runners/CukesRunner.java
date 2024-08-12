57557d68562b4a1822885c71be134d660db35ed2
peace <mehmetakkaya1990@gmail.com>
2024-08-12 10:27:30 +0300

    first

--- src/test/java/com/runners/CukesRunner.java
+++ src/test/java/com/runners/CukesRunner.java
@@ -7,13 +7,14 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-report.html",
-            "json:target/cucumber-reports/cucumber.json"},
+            "json:target/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/step_definitons",
         dryRun = false, // true means that do not run my step definitions but only check if any step is missing snippet.
         tags = "@employee"
-)
 
+
+)
 public class CukesRunner {
 
 }
