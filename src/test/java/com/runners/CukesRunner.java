805b5523d6aae02c2be852c82cc935584c03c65d
peace <mehmetakkaya1990@gmail.com>
2024-08-12 10:38:44 +0300

    first

--- src/test/java/com/runners/CukesRunner.java
+++ src/test/java/com/runners/CukesRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"html:target/cucumber-report.html",
-            "json:target/cucumber.json"},
+            "json:target/cucumber-reports/cucumber.json"},
         features = "src/test/resources/features",
         glue = "com/step_definitons",
         dryRun = false, // true means that do not run my step definitions but only check if any step is missing snippet.
